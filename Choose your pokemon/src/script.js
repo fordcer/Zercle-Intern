11cd34b6e014cca11c1d28bcadd0ae73894d2a85
fordcer <76239586+fordcer@users.noreply.github.com>
2022-05-12 16:41:39 +0700

    80% cal

--- Choose your pokemon/src/script.js
+++ Choose your pokemon/src/script.js
@@ -1,13 +1,16 @@
-let url = "https://pokeapi.co/api/v2/pokemon/1"
+let url = "https://pokeapi.co/api/v2/pokemon/"
+let num = Math.floor(Math.random() * 100) + 1;
 
-fetch(url)
+fetch(url+num)
     .then(function (response) {
         return response.json();
     }) 
     .then(function (data) {
         console.log(data);
         console.log("This name "+ data.species.name);
-        console.log("This data"+ data.length);
+        console.log("This data"+ data.sprites);
+        pokemonShufflePic(data);
+        pokemonPic(data);
         pokemonData(data);
     })
     .catch(function (err) {
@@ -16,11 +19,68 @@ fetch(url)
 
 
 function pokemonData(data) {
-    var mainContainer = document.getElementById("pokemon");
-    for (var i = 0; i < data.length; i++) {
-        var div = document.createElement("div");
-        // div.innerHTML = `Name : ${data.species.name}`;
-        div.innerHTML = "Name : " + data.species.name;
-        mainContainer.appendChild(div); 
-    }
+    var nameContainer = document.getElementById("pokemon-name");
+    var weightContainer = document.getElementById("pokemon-weight");
+    var heightContainer = document.getElementById("pokemon-height");
+    var pokemon_id = data.id
+    var pokemon_weight = data.weight
+    var pokemon_height = data.height
+    var pokemon_name = data.species.name[0].toUpperCase() + data.species.name.substring(1);
+
+    nameContainer.innerHTML = `${pokemon_name} #${pokemon_id}`;
+    weightContainer.innerHTML = `Weight: ${pokemon_weight}`;
+    heightContainer.innerHTML = `Height: ${pokemon_height}`;
+}
+
+function pokemonPic(data) {
+    var picContainerLeft = document.getElementById("pic_left");
+    var picContainerRight = document.getElementById("pic_right");
+    var picPokemon = data.sprites;
+
+    var topLeft = document.createElement("img")
+    var bottomLeft = document.createElement("img")
+    var topRight = document.createElement("img")
+    var bottomRight = document.createElement("img")
+
+    topLeft.setAttribute("src", picPokemon.front_default);
+    bottomLeft.setAttribute("src", picPokemon.back_default)
+    topRight.setAttribute("src", picPokemon.front_shiny)
+    bottomRight.setAttribute("src", picPokemon.back_shiny)
+
+    topLeft.setAttribute("class","all_pokemon");
+    bottomLeft.setAttribute("class","all_pokemon");
+    topRight.setAttribute("class","all_pokemon");
+    bottomRight.setAttribute("class","all_pokemon");
+
+    picContainerLeft.appendChild(topLeft);
+    picContainerLeft.appendChild(bottomLeft);
+    picContainerRight.appendChild(topRight);
+    picContainerRight.appendChild(bottomRight);
+}
+
+function pokemonShufflePic(data) {
+    var mainContainer = document.getElementById("pic_main");
+    var picpokemon = data.sprites;
+    var shuffleArray = [];
+    var count = 0;
+
+    var mainPicPokemon = document.createElement("img");
+
+    // mainPicPokemon.setAttribute("src", picpokemon.front_default);
+    mainPicPokemon.setAttribute("class","all_pokemon");
+    mainContainer.appendChild(mainPicPokemon);
+
+    shuffleArray.push(picpokemon.front_default);
+    shuffleArray.push(picpokemon.back_default);
+    shuffleArray.push(picpokemon.front_shiny);
+    shuffleArray.push(picpokemon.back_shiny);
+
+    setInterval(function() {
+        mainPicPokemon.setAttribute("src",shuffleArray[count]);
+        count++;
+        if (count == 4) {
+            count = 0
+        }
+    }, 2000);
+
 }
--- Choose your pokemon/tailwind.config.js
+++ Choose your pokemon/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'pokemon-bg' : "url('../src/asset/poke_bg.jpg')"
+      }
+    },
   },
   plugins: [],
 }
